85aa1271a52d221360bd825f70412d3f24a219be
yoieh <yoieh@live.se>
2016-09-16 11:33:15 +0200

    fixt source

--- test_yColor.sh
+++ test_yColor.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+declare yColor_dir
+
 function IMPORT_yColor {
     yoieh_DIR1="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)"
-    yoieh_DIR1="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && cd .. && pwd)"
+    yoieh_DIR2="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && cd .. && pwd)"
     yColor_DIR1=${yoieh_DIR1}"/yColor/"
     yColor_DIR2=${yoieh_DIR2}"/yColor/"
 
     if [ -d "${yColor_DIR1}" ]; then
         source ${yColor_DIR1}"yColor.sh";
+	echo ${yColor_DIR1}
+	yColor_dir=${yColor_DIR1}
         return 0
     elif [ -d "${yColor_DIR2}" ]; then
         source ${yColor_DIR2}"yColor.sh";
+	yColor_dir=${yColor_DIR2}
         return 0
     else
         return 1
@@ -19,17 +24,22 @@ function IMPORT_yColor {
 
 function IF_yColor {
     if IMPORT_yColor; then
+	source ${yColor_dir}"yColor.sh";
         printf "%syColor is installed in :\n %s%s%s\n"\
                 "${yc[GR_B]}"\
                 "${yc[GR_U]}"\
-                "${yColor_DIR}"\
+                "${yColor_dir}"\
                 "${yc[RESET]}"
+	return 0
     else
-        printf "Error: yColor not installd in :\n %s\n"\
-                "${yColor_DIR}"
+        printf "Error: yColor not installd :\n %s\n"\
+                "${yColor_dir}"
+	return 1
     fi
 }
 
-IF_yColor
+if IF_yColor; then
+	source ${yColor_dir}"yColor.sh";
+fi
 
 exit 0
